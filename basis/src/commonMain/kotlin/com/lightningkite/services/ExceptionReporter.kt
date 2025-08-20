d89c04df72547b1a48e35fe3f299b6cda8c718b2
Joseph Ivie <joseph@lightningkite.com>
2025-08-20 10:38:03 -0600

    Exception reporter settings fix

--- basis/src/commonMain/kotlin/com/lightningkite/services/ExceptionReporter.kt
+++ basis/src/commonMain/kotlin/com/lightningkite/services/ExceptionReporter.kt
@@ -1,11 +1,14 @@
 package com.lightningkite.services
 
+import kotlinx.serialization.Serializable
+import kotlin.jvm.JvmInline
+
 public interface ExceptionReporter : Service {
 
     /**
      * The function that makes the reports to the underlying service.
      */
-    public suspend fun report(t: Throwable, context: Any? = null): Boolean
+    public suspend fun report(t: Throwable, context: Any? = null)
 
     /**
      * Will attempt to send a report to confirm that the service is alive and available.
@@ -16,12 +19,34 @@ public interface ExceptionReporter : Service {
         } catch (e: Exception) {
             return HealthStatus(HealthStatus.Level.ERROR, additionalMessage = e.message)
         }
-        return if (report)
-            HealthStatus(HealthStatus.Level.OK)
-        else
-            HealthStatus(
-                HealthStatus.Level.WARNING,
-                additionalMessage = "Disabled"
-            )
+        return HealthStatus(HealthStatus.Level.OK)
+    }
+
+
+    @Serializable
+    @JvmInline
+    public value class Settings(
+        public val url: String = "none"
+    ) : Setting<ExceptionReporter> {
+
+        public companion object : UrlSettingParser<ExceptionReporter>() {
+            init {
+                register("none") { name, url, context -> None(name, context) }
+            }
+        }
+
+        override fun invoke(name: String, context: SettingContext): ExceptionReporter {
+            return parse(name, url, context)
+        }
+    }
+
+    public class None(override val name: String, override val context: SettingContext): ExceptionReporter {
+        override suspend fun report(t: Throwable, context: Any?) {
+        }
+        override suspend fun healthCheck(): HealthStatus = HealthStatus(
+            level = HealthStatus.Level.WARNING,
+            additionalMessage = "No metrics will be reported.",
+            checkedAt = context.clock.now()
+        )
     }
 }
