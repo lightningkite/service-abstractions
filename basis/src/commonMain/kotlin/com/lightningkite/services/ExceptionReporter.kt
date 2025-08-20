48291a9160ced7ae29e36dd9a9c05d78a8571564
Joseph Ivie <joseph@lightningkite.com>
2025-08-20 16:00:58 -0600

    log exception reporter

--- basis/src/commonMain/kotlin/com/lightningkite/services/ExceptionReporter.kt
+++ basis/src/commonMain/kotlin/com/lightningkite/services/ExceptionReporter.kt
@@ -1,5 +1,7 @@
 package com.lightningkite.services
 
+import io.github.oshai.kotlinlogging.KLogger
+import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.Serializable
 import kotlin.jvm.JvmInline
 
@@ -8,14 +10,14 @@ public interface ExceptionReporter : Service {
     /**
      * The function that makes the reports to the underlying service.
      */
-    public suspend fun report(t: Throwable, context: Any? = null)
+    public suspend fun report(t: Throwable, context: String)
 
     /**
      * Will attempt to send a report to confirm that the service is alive and available.
      */
     override suspend fun healthCheck(): HealthStatus {
         val report = try {
-            report(Exception("Health Check: Can Report Exception"))
+            report(Exception("Health Check: Can Report Exception"), "HEALTH CHECK")
         } catch (e: Exception) {
             return HealthStatus(HealthStatus.Level.ERROR, additionalMessage = e.message)
         }
@@ -32,6 +34,7 @@ public interface ExceptionReporter : Service {
         public companion object : UrlSettingParser<ExceptionReporter>() {
             init {
                 register("none") { name, url, context -> None(name, context) }
+                register("log") { name, url, context -> Logging(name, context) }
             }
         }
 
@@ -41,11 +44,23 @@ public interface ExceptionReporter : Service {
     }
 
     public class None(override val name: String, override val context: SettingContext): ExceptionReporter {
-        override suspend fun report(t: Throwable, context: Any?) {
+        override suspend fun report(t: Throwable, context: String) {
         }
         override suspend fun healthCheck(): HealthStatus = HealthStatus(
             level = HealthStatus.Level.WARNING,
             additionalMessage = "No metrics will be reported.",
+            checkedAt = context.clock.now()
+        )
+    }
+
+    public class Logging(override val name: String, override val context: SettingContext): ExceptionReporter {
+        private val logs = KotlinLogging.logger("com.lightningkite.services.ExceptionReporter.$name")
+        override suspend fun report(t: Throwable, context: String) {
+            logs.error(t) { context }
+        }
+        override suspend fun healthCheck(): HealthStatus = HealthStatus(
+            level = HealthStatus.Level.WARNING,
+            additionalMessage = "No metrics will be reported other than the logs",
             checkedAt = context.clock.now()
         )
     }
