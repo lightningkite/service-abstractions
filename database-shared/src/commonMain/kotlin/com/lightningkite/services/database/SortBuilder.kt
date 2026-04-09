3f859dfeba5c356a1b7a3dd9fe96a58a68be2c5f
iHoonter <huntdawghero@gmail.com>
2026-04-09 15:00:40 -0600

    JVM name fixes

--- database-shared/src/commonMain/kotlin/com/lightningkite/services/database/SortBuilder.kt
+++ database-shared/src/commonMain/kotlin/com/lightningkite/services/database/SortBuilder.kt
@@ -1,5 +1,7 @@
 package com.lightningkite.services.database
 
+import kotlin.jvm.JvmName
+
 public inline fun <reified T> sort(setup: SortBuilder<T>.(DataClassPath<T, T>) -> Unit): List<SortPart<T>> {
     return SortBuilder<T>().apply {
         setup(this, path())
@@ -18,6 +20,7 @@ public class SortBuilder<K>() {
     public fun <V : Comparable<V>> DataClassPath<K, V>.ascending(): Unit = sort(true)
     public fun <V : Comparable<V>> DataClassPath<K, V>.descending(): Unit = sort(false)
 
+    @JvmName("stringSort")
     public fun DataClassPath<K, String>.sort(ascending: Boolean, ignoreCase: Boolean): Unit = add(SortPart(this, ascending, ignoreCase))
     public fun DataClassPath<K, String>.ascending(ignoreCase: Boolean): Unit = sort(true, ignoreCase)
     public fun DataClassPath<K, String>.descending(ignoreCase: Boolean): Unit = sort(false, ignoreCase)
@@ -26,9 +29,12 @@ public class SortBuilder<K>() {
     public annotation class SortByEnumName
 
     @SortByEnumName
+    @JvmName("enumSort")
     public fun <V : Enum<V>> DataClassPath<K, V>.sort(ascending: Boolean): Unit = add(SortPart(this, ascending))
     @SortByEnumName
+    @JvmName("enumAscending")
     public fun <V : Enum<V>> DataClassPath<K, V>.ascending(): Unit = sort(true)
     @SortByEnumName
+    @JvmName("enumDescending")
     public fun <V : Enum<V>> DataClassPath<K, V>.descending(): Unit = sort(false)
 }
